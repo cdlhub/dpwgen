d6f6389d430dc5c352f991e963db42a09f35aab3
Camille Daum-Lobko <camille@daum-lobko.com>
2019-02-27 12:07:47 -0500

    Add main go files

---
+++ main.go
@@ -0,0 +1,70 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	"log"
+	"os"
+	"strings"
+)
+
+// Options is application command line options
+type Options struct {
+	version      bool
+	n            uint
+	d            uint
+	passFileName string
+}
+
+// Logger is all loggers
+type Logger struct {
+	Info    *log.Logger
+	Warning *log.Logger
+	Error   *log.Logger
+}
+
+var (
+	options Options
+	logger  Logger
+)
+
+func init() {
+	initOptions(&options)
+	initLogger(&logger)
+}
+
+func initOptions(opt *Options) {
+	flag.Usage = func() {
+		fmt.Fprintln(os.Stdout, "Usage:")
+		fmt.Fprintf(os.Stdout, "\t%s -version\n", APPNAME)
+		fmt.Fprintf(os.Stdout, "\t%s [ -n N ] [-d D] <pass-file-name> \n", APPNAME)
+		fmt.Fprintln(os.Stdout)
+
+		flag.PrintDefaults()
+	}
+
+	flag.BoolVar(&opt.version, "version", false, "Print version number")
+	flag.UintVar(&opt.n, "n", 6, "the number of words in the password")
+	flag.UintVar(&opt.d, "d", 5, "the number of dice to roll to select a word")
+
+	flag.Parse()
+	opt.passFileName = strings.Join(flag.Args(), " ")
+}
+
+func initLogger(logger *Logger) {
+	*logger = Logger{
+		Info:    log.New(os.Stderr, "INFO: ", 0),
+		Warning: log.New(os.Stderr, "WARNING: ", 0),
+		Error:   log.New(os.Stderr, "ERROR: ", 0),
+	}
+}
+
+func main() {
+	opt := options
+	// log := logger
+
+	if opt.version {
+		fmt.Println(APPNAME + " version " + VERSION)
+	}
+	os.Exit(0)
+}
