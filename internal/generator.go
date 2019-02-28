166100f96e813e9637b02c2ab386bf6e6432bab1
Camille Daum-Lobko <camille@daum-lobko.com>
2019-02-28 10:09:36 -0500

    Add loadWordList and implement GeneratePassword

--- internal/generator.go
+++ internal/generator.go
@@ -4,7 +4,7 @@ import (
 	"bufio"
 	"fmt"
 	"io"
-	"strconv"
+	"regexp"
 	"strings"
 	"unicode/utf8"
 )
@@ -13,26 +13,63 @@ import (
 //
 // n is the number of words the password is made of.
 func GeneratePassword(wordList io.Reader, n uint) (string, error) {
-	scanner := bufio.NewScanner(wordList)
-	if !scanner.Scan() {
-		return "", fmt.Errorf("empty word list")
-	}
-	text := scanner.Text()
-	fields := strings.Fields(text)
-	if len(fields) != 2 {
-		return "", fmt.Errorf("word list bad format: must be 'nnnn password': is: %s", text)
+	wl, d, err := loadWordList(wordList)
+	if err != nil {
+		return "", fmt.Errorf("generate password: %v", err)
 	}
 
-	id := fields[0]
-	if _, err := strconv.ParseUint(id, 0, 64); err != nil {
-		return "", fmt.Errorf("word list bad format: password id is not unsigned int: %s", id)
+	var pw []string
+	for i := uint(0); i < n; i++ {
+		w, err := getRandomWord(wl, d)
+		if err != nil {
+			return w, fmt.Errorf("generate password: %v", err)
+		}
+		pw = append(pw, w)
 	}
 
-	d := utf8.RuneCountInString(id)
-	return getPassword(wordList, d)
+	return strings.Join(pw, " "), nil
 }
 
-func getPassword(wordList io.Reader, d int) (string, error) {
+func loadWordList(wordList io.Reader) (map[string]string, int, error) {
+	wl := make(map[string]string)
+	nDice := -1
+
+	scanner := bufio.NewScanner(wordList)
+	for i := uint(1); scanner.Scan(); i++ {
+		text := scanner.Text()
+		fields := strings.Fields(text)
+		if len(fields) != 2 {
+			return nil, -1, fmt.Errorf("word list: line %d: bad format: must be 'nnnn password': is: %s", i, text)
+		}
+
+		d := utf8.RuneCountInString(fields[0])
+		if nDice == -1 {
+			nDice = d
+		}
+		if err := checkID(fields[0], nDice); err != nil {
+			return nil, -1, fmt.Errorf("word list: line %d: %v", i, err)
+		}
+
+		wl[fields[0]] = fields[1]
+	}
+
+	return wl, nDice, nil
+}
+
+func checkID(id string, n int) error {
+	r := regexp.MustCompile("^[123456]+$")
+	if !r.MatchString(id) {
+		return fmt.Errorf("id bad format: should be composed of 1, 2, 3, 4, 5, or 6 only: is: %s", id)
+	}
+
+	if utf8.RuneCountInString(id) != n {
+		return fmt.Errorf("id bad format: should be of length %d: length is: %d", n, len(id))
+	}
+
+	return nil
+}
+
+func getRandomWord(wl map[string]string, d int) (string, error) {
 
 	return "", nil
 }
